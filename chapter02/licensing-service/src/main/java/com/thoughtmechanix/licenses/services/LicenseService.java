e262d4d2d563b307082ebcf09c0606c79f94ead4
ifjso <ojsboy@hotmail.com>
2019-07-14 20:42:22 +0900

    Create services

---
+++ chapter02/licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -0,0 +1,29 @@
+package com.thoughtmechanix.licenses.services;
+
+import com.thoughtmechanix.licenses.model.License;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LicenseService {
+
+  public License getLicense(String licenseId) {
+    return new License()
+        .withId(licenseId)
+        .withOrganizationId(UUID.randomUUID().toString())
+        .withProductName("Test Product Name")
+        .withLicenseType("PerSeat");
+  }
+
+  public void saveLicense(License license) {
+
+  }
+
+  public void updateLicense(License license) {
+
+  }
+
+  public void deleteLicense(License license) {
+
+  }
+}
