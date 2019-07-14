7da1355d9c159ed52c7b9f67e0329a1ea681c966
ifjso <ojsboy@hotmail.com>
2019-07-14 20:46:24 +0900

    Add put, post, delete

--- chapter02/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ chapter02/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -1,13 +1,15 @@
 package com.thoughtmechanix.licenses.controllers;
 
 import com.thoughtmechanix.licenses.model.License;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "v1/organizations/{organizationId}/licenses")
+@RequestMapping(value = "/v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
 
   @RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
@@ -17,5 +19,21 @@ public class LicenseServiceController {
         .withOrganizationId(organizationId)
         .withProductName("Teleco")
         .withLicenseType("Seat");
+  }
+
+  @RequestMapping(value = "/{licenseId}", method = RequestMethod.PUT)
+  public String updateLicense(@PathVariable String licenseId) {
+    return String.format("This is the put");
+  }
+
+  @RequestMapping(value = "/{licenseId}", method = RequestMethod.POST)
+  public String saveLicense(@PathVariable String licenseId) {
+    return String.format("This is the post");
+  }
+
+  @RequestMapping(value = "/{licenseId}", method = RequestMethod.DELETE)
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public String deleteLicense(@PathVariable String licenseId) {
+    return String.format("This is the delete");
   }
 }
