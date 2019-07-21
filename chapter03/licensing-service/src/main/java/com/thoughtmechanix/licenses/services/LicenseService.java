e40f4742493d80d2b7491dae3f6f8a6ff7e356ee
ifjso <ojsboy@hotmail.com>
2019-07-21 16:49:22 +0900

    license service 추가

---
+++ chapter03/licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -0,0 +1,36 @@
+package com.thoughtmechanix.licenses.services;
+
+import com.thoughtmechanix.licenses.model.License;
+import com.thoughtmechanix.licenses.repository.LicenseRepository;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LicenseService {
+
+  @Autowired
+  private LicenseRepository licenseRepository;
+
+  public License getLicense(String organizationId, String licenseId) {
+    return licenseRepository.findByOrOrganizationIdAndLicenseId(organizationId, licenseId);
+  }
+
+  public List<License> getLicensesByOrg(String organizationId) {
+    return licenseRepository.findByOrganizationId(organizationId);
+  }
+
+  public void saveLicense(License license) {
+    license.withId(UUID.randomUUID().toString());
+    licenseRepository.save(license);
+  }
+
+  public void updateLicense(License license) {
+    licenseRepository.save(license);
+  }
+
+  public void deleteLicense(License license) {
+    licenseRepository.delete(license);
+  }
+}
