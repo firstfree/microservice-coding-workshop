b659de7040f8f693d67593db072287c08695cd14
ifjso <ojsboy@hotmail.com>
2019-07-23 22:18:29 +0900

    controller 구현

---
+++ chapter04/organization-service/src/main/java/com/thoughtmechanix/organization/controllers/OrganizationServiceController.java
@@ -0,0 +1,46 @@
+package com.thoughtmechanix.organization.controllers;
+
+import com.thoughtmechanix.organization.model.Organization;
+import com.thoughtmechanix.organization.services.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/organizations")
+public class OrganizationServiceController {
+
+  @Autowired
+  private OrganizationService organizationService;
+
+  @GetMapping("/{organizationId}")
+  public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
+    return organizationService.getOrganization(organizationId);
+  }
+
+  @PutMapping("/{organizationId}")
+  public void updateOrganization(@PathVariable("organizationId") String organizationId,
+      @RequestBody Organization organization) {
+    organizationService.updateOrganization(organization);
+  }
+
+  @PostMapping(value = "/{organizationId}")
+  public void saveOrganiztion(@RequestBody Organization organization) {
+    organizationService.saveOrganization(organization);
+  }
+
+  @DeleteMapping(value = "/organizationId")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteOrganization(@PathVariable("organizationId") String organizationId,
+      @RequestBody Organization organization) {
+    organizationService.deleteOrganization(organization);
+  }
+}
