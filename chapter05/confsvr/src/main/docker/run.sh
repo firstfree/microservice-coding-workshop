c1d82cb6e66b491841f2a1234a0c74d742a586ff
ifjso <ojsboy@hotmail.com>
2019-08-03 19:44:03 +0900

    config server run.sh 오류 수정

--- chapter05/confsvr/src/main/docker/run.sh
+++ chapter05/confsvr/src/main/docker/run.sh
@@ -9,4 +9,4 @@ echo "********************************************************"
 echo "Starting Configuration Service with Eureka Endpoint: $EUREKASERVER_URI"
 echo "********************************************************"
 
-java -Djava.security.egd=file:/dev/./urandom -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -jar /usr/local/configserver/@project.build/finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -jar /usr/local/configserver/@project.build.finalName@.jar
