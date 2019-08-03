dfb35f8e9ade5218be40eb4c9eb7e122bbfb99b0
ifjso <ojsboy@hotmail.com>
2019-08-03 19:11:25 +0900

    feign, eureka client 사용 추가

--- chapter05/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ chapter05/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -2,12 +2,15 @@ package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
