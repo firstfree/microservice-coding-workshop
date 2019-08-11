590c77290be46e4340145acb533fb116dc945bdc
ifjso <ojsboy@hotmail.com>
2019-08-12 01:50:15 +0900

    license repository 추가

---
+++ chapter06/lincensing-service/src/main/java/com/thoughtmechanix/lincenses/repository/LicenseRepository.java
@@ -0,0 +1,14 @@
+package com.thoughtmechanix.lincenses.repository;
+
+import com.thoughtmechanix.lincenses.model.License;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LicenseRepository extends CrudRepository<License, String> {
+
+  List<License> findByOrganizationId(String organizationId);
+
+  License findByOrganizationIdAndLicenseId(String organizationId, String licenseId);
+}
