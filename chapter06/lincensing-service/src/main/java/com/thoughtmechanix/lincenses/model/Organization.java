52ad4bb0d17918ceb14b7ba8abba450cdc7d68aa
ifjso <ojsboy@hotmail.com>
2019-08-12 01:46:47 +0900

    model 추가

---
+++ chapter06/lincensing-service/src/main/java/com/thoughtmechanix/lincenses/model/Organization.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.lincenses.model;
+
+import lombok.Data;
+
+@Data
+public class Organization {
+
+  private String id;
+  private String name;
+  private String contactName;
+  private String contactEmail;
+  private String contactPhone;
+}
