33cb9169ec01e49aa005d49e43e86f4a61322f5d
ifjso <ojsboy@hotmail.com>
2019-08-11 15:37:53 +0900

    config server run.sh 수정

--- chapter06/confsvr/src/main/docker/run.sh
+++ chapter06/confsvr/src/main/docker/run.sh
@@ -2,7 +2,7 @@
 
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
-while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep3; done
+while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep 3; done
 echo ">>>>>>>>>>>> Eureka Server has started"
 
 echo "********************************************************"
