8acb6e7e9837dce6b151360bdfbbc67f008da2a6
ifjso <ojsboy@hotmail.com>
2019-08-11 13:59:34 +0900

    config server docker 설정 추가

---
+++ chapter06/confsvr/src/main/docker/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
+while ! `nc -z eurekaserver $EUREKASERVER_PORT`; do sleep3; done
+echo ">>>>>>>>>>>> Eureka Server has started"
+
+echo "********************************************************"
+echo "Starting Configuration Service with Eureka Endpoint: $EUREKASERVER_URI"
+echo "********************************************************"
+
+java -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI -jar /usr/local/configserver/@project.build.finalName@.jar
+
