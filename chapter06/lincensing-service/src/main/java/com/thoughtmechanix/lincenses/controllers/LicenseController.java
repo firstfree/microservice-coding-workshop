77358d28c07dc6b4cc913761ab80fe22e708a902
ifjso <ojsboy@hotmail.com>
2019-08-13 03:26:58 +0900

    license controller 추가

---
+++ chapter06/lincensing-service/src/main/java/com/thoughtmechanix/lincenses/controllers/LicenseController.java
@@ -0,0 +1,29 @@
+package com.thoughtmechanix.lincenses.controllers;
+
+import com.thoughtmechanix.lincenses.model.License;
+import com.thoughtmechanix.lincenses.services.LicenseService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/organizations/{organizationId}/licenses")
+public class LicenseController {
+
+  private final LicenseService licenseService;
+
+  @GetMapping
+  public List<License> getLicenses(@PathVariable("organizationId") String organizationId) {
+    return licenseService.getLicenses(organizationId);
+  }
+
+  @GetMapping("/{licenseId}")
+  public License getLicense(@PathVariable("organizationId") String organizationId,
+      @PathVariable("licenseId") String licenseId) {
+    return licenseService.getLicense(organizationId, licenseId);
+  }
+}
