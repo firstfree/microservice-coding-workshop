f2030f1a4a9b241dc7109c9465376294f74a274d
ifjso <ojsboy@hotmail.com>
2019-08-13 19:18:20 +0900

    hystrix concurrency strategy custom 적용

--- chapter06/licensing-service/src/main/java/com/thoughtmechanix/licenses/utils/UserContextHolder.java
+++ chapter06/licensing-service/src/main/java/com/thoughtmechanix/licenses/utils/UserContextHolder.java
@@ -1,11 +1,13 @@
 package com.thoughtmechanix.licenses.utils;
 
+import org.springframework.util.Assert;
+
 public class UserContextHolder {
 
   public static final ThreadLocal<UserContext> userContext = new ThreadLocal<>();
 
   public static final UserContext getContext() {
-    UserContext  context = userContext.get();
+    UserContext context = userContext.get();
 
     if (context == null) {
       context = createEmptyContext();
@@ -17,5 +19,10 @@ public class UserContextHolder {
 
   public static final UserContext createEmptyContext() {
     return new UserContext();
+  }
+
+  public static final void setContext(UserContext context) {
+    Assert.notNull(context, "Only non-null UserContext instance are permitted");
+    userContext.set(context);
   }
 }
