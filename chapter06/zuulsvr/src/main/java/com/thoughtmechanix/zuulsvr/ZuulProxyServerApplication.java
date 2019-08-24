14fb712d47379317987e2da76815889bd1baa00f
ifjso <ojsboy@hotmail.com>
2019-08-24 13:23:03 +0900

    resttemplate에 interceptor 등록

--- chapter06/zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulProxyServerApplication.java
+++ chapter06/zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulProxyServerApplication.java
@@ -1,12 +1,33 @@
 package com.thoughtmechanix.zuulsvr;
 
+import com.thoughtmechanix.zuulsvr.utils.UserContextInterceptor;
+import java.util.Collections;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulProxyServerApplication {
+
+  @Bean
+  @LoadBalanced
+  public RestTemplate getRestTemplate() {
+    RestTemplate restTemplate = new RestTemplate();
+    List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+    if (interceptors == null) {
+      restTemplate.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+    } else {
+      interceptors.add(new UserContextInterceptor());
+    }
+
+    return restTemplate;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ZuulProxyServerApplication.class, args);
