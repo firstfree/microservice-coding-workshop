ba5efdd6fae7ec8d27eb8bc518d9fc24271bbdf3
ifjso <ojsboy@hotmail.com>
2019-08-24 18:29:24 +0900

    special routes service 추가

---
+++ chapter06/specialroutes-service/src/main/java/com/thoughtmechanix/specialroutes/EagleEyeSpecialRoutesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtmechanix.specialroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyeSpecialRoutesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleEyeSpecialRoutesServiceApplication.class, args);
+	}
+}
