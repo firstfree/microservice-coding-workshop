a7288f2f1b5c078ae83a034484286e6ba71a6958
ifjso <ojsboy@hotmail.com>
2019-08-26 08:49:25 +0900

    license service에서 zuul을 통해 organization service api를 호출하도록 수정

--- chapter06/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
+++ chapter06/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -2,12 +2,15 @@ package com.thoughtmechanix.licenses.clients;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thoughtmechanix.licenses.model.Organization;
+import com.thoughtmechanix.licenses.utils.UserContextHolder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrganizationRestTemplateClient {
@@ -16,8 +19,11 @@ public class OrganizationRestTemplateClient {
 
   @HystrixCommand
   public Organization getOrganization(String organizationId) {
+    log.debug(">>> In Licensing Service.getOrganization: {}, Thread id: {}",
+        UserContextHolder.getContext().getCorrelationId(), Thread.currentThread().getId());
+    
     ResponseEntity<Organization> restExchange = restTemplate.exchange(
-        "http://organizationservice/v1/organizations/{organizationId}",
+        "http://zuulservice/api/organization/v1/organizations/{organizationId}",
         HttpMethod.GET,
         null, Organization.class, organizationId);
     return restExchange.getBody();
