d03e62f206e581f1aee1f4032843cf0ee9d73756
ifjso <ojsboy@hotmail.com>
2019-08-29 09:40:18 +0900

    correlation id 전달하도록 수정

--- chapter06/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
+++ chapter06/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -2,10 +2,15 @@ package com.thoughtmechanix.licenses.clients;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thoughtmechanix.licenses.model.Organization;
+import com.thoughtmechanix.licenses.utils.UserContext;
 import com.thoughtmechanix.licenses.utils.UserContextHolder;
+import javax.print.DocFlavor.STRING;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -22,10 +27,14 @@ public class OrganizationRestTemplateClient {
     log.debug(">>> In Licensing Service.getOrganization: {}, Thread id: {}",
         UserContextHolder.getContext().getCorrelationId(), Thread.currentThread().getId());
 
+    HttpHeaders headers = new HttpHeaders();
+    headers.set(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
+    HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
     ResponseEntity<Organization> restExchange = restTemplate.exchange(
         "http://zuulservice/api/organization/v1/organizations/{organizationId}",
         HttpMethod.GET,
-        null, Organization.class, organizationId);
+        httpEntity, Organization.class, organizationId);
     return restExchange.getBody();
   }
 }
