a44f486a3bc8104c4a20cb99ebd833b8bcdd1628
ifjso <ojsboy@hotmail.com>
2019-08-29 09:40:48 +0900

    correlation id 받도록 수정

--- chapter06/organization-service/src/main/java/com/thoughtmechanix/organization/controllers/OrganizationController.java
+++ chapter06/organization-service/src/main/java/com/thoughtmechanix/organization/controllers/OrganizationController.java
@@ -3,15 +3,18 @@ package com.thoughtmechanix.organization.controllers;
 import com.thoughtmechanix.organization.model.Organization;
 import com.thoughtmechanix.organization.services.OrganizationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/organizations")
 @RequiredArgsConstructor
@@ -20,7 +23,10 @@ public class OrganizationController {
   private final OrganizationService organizationService;
 
   @GetMapping("/{organizationId}")
-  public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
+  public Organization getOrganization(@RequestHeader("tmx-correlation-id") String correlationId,
+      @PathVariable("organizationId") String organizationId) {
+    log.debug("tmx-correlation-id: {}", correlationId);
+
     Organization organization = organizationService.getOrganization(organizationId);
     organization.setContactName("OLD::" + organization.getContactName());
     return organization;
