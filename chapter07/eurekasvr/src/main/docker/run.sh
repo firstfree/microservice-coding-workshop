cd5cc61531d9cd31a3fd77b57e8a964e2238c340
ifjso <ojsboy@hotmail.com>
2019-09-08 19:45:45 +0900

    eureka server docker 설정 추가

---
+++ chapter07/eurekasvr/src/main/docker/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "Starting the Eureka Server"
+echo "********************************************************"
+java -jar /usr/local/eurekaserver/@project.build.finalName@.jar
+
