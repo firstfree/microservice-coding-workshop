00bada9c16f4f3216d5281e94e2f150a387200f6
ifjso <ojsboy@hotmail.com>
2019-09-14 20:50:22 +0900

    organization을 위한 feign client 추가

--- chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
+++ chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
@@ -3,23 +3,14 @@ package com.thoughtmechanix.licenses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
+@EnableFeignClients
 @EnableCircuitBreaker
 public class EagleEyeLicensingServiceApplication {
-
-	@LoadBalanced
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EagleEyeLicensingServiceApplication.class, args);
---
+++ chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationFeignClient.java
@@ -0,0 +1,16 @@
+package com.thoughtmechanix.licenses.clients;
+
+import com.thoughtmechanix.licenses.model.Organization;
+import com.thoughtmechanix.licenses.utils.UserContext;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient("zuulservice")
+public interface OrganizationFeignClient {
+
+  @GetMapping("/api/organization/v1/organizations/{organizationId}")
+  Organization getOrganization(@RequestHeader(UserContext.CORRELATION_ID) String correlationId,
+      @PathVariable String organizationId);
+}
