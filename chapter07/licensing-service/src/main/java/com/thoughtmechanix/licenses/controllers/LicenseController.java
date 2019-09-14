2d0e1bf2e13cf4fba6770045f7f6fbf09089ec02
ifjso <ojsboy@hotmail.com>
2019-09-15 02:12:50 +0900

    license controller 추가

---
+++ chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseController.java
@@ -0,0 +1,38 @@
+package com.thoughtmechanix.licenses.controllers;
+
+import com.thoughtmechanix.licenses.model.License;
+import com.thoughtmechanix.licenses.services.LicenseService;
+import com.thoughtmechanix.licenses.utils.UserContextHolder;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/organizations/{organizationId}/licenses")
+public class LicenseController {
+
+  private final LicenseService licenseService;
+
+  @GetMapping
+  public List<License> getLicenses(@PathVariable("organizationId") String organizationId) {
+    log.debug("LicenseController correlation id: {}",
+        UserContextHolder.getContext().getCorrelationId());
+    return licenseService.getLicenses(organizationId);
+  }
+
+  @GetMapping("/{licenseId}")
+  public License getLicense(@RequestHeader("tmx-correlation-id") String correlationId,
+      @PathVariable("organizationId") String organizationId,
+      @PathVariable("licenseId") String licenseId) {
+    log.debug("LicenseController correlation id: {}, thread id: {}", correlationId,
+        Thread.currentThread().getId());
+    return licenseService.getLicense(organizationId, licenseId);
+  }
+}
