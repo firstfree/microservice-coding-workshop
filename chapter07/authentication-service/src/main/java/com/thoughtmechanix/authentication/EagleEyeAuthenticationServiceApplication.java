f40b7797be2fb783c00fe0b45cb37abdd4b3a8c1
ifjso <ojsboy@hotmail.com>
2019-09-21 22:54:17 +0900

    authentication service first commit

---
+++ chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyeAuthenticationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EagleEyeAuthenticationServiceApplication.class, args);
+  }
+
+}
