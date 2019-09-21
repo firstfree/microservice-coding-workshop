2f1b51844aa84a7facd1e9e63c995dd0c048b7eb
ifjso <ojsboy@hotmail.com>
2019-09-21 23:40:27 +0900

    user endpoint 추가

--- chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
+++ chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
@@ -1,15 +1,28 @@
 package com.thoughtmechanix.authentication;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 public class EagleEyeAuthenticationServiceApplication {
 
+  @GetMapping(value = "/user")
+  public Map<String, Object> user(OAuth2Authentication user) {
+    Map<String, Object> userInfo = new HashMap<>();
+    userInfo.put("user", user.getUserAuthentication().getPrincipal());
+    userInfo.put("authorities", AuthorityUtils.authorityListToSet(
+        user.getUserAuthentication().getAuthorities()));
+    return userInfo;
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(EagleEyeAuthenticationServiceApplication.class, args);
   }
-
 }
