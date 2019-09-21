2b3e994f5f5f71ffc2c2b2b698787bde4ad73f66
ifjso <ojsboy@hotmail.com>
2019-09-21 23:33:05 +0900

    enable authorization server annotation 추가

--- chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
+++ chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
@@ -2,8 +2,10 @@ package com.thoughtmechanix.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class EagleEyeAuthenticationServiceApplication {
 
   public static void main(String[] args) {
