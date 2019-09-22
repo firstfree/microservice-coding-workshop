ae99e8983395ee5d02a6ca915e9dfe3f124cfe94
ifjso <ojsboy@hotmail.com>
2019-09-22 12:47:49 +0900

    RestController annotation 추가

--- chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
+++ chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/EagleEyeAuthenticationServiceApplication.java
@@ -8,8 +8,10 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableAuthorizationServer
 public class EagleEyeAuthenticationServiceApplication {
 
