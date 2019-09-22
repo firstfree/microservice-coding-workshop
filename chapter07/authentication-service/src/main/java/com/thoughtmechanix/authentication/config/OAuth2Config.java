a07aaa7610fc0d7653d9e10fa861418bc26aa068
ifjso <ojsboy@hotmail.com>
2019-09-22 13:44:22 +0900

    resource에 접근할 client 설정

--- chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/config/OAuth2Config.java
+++ chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/config/OAuth2Config.java
@@ -1,30 +1,35 @@
 package com.thoughtmechanix.authentication.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
-public class OAuth2Config implements AuthorizationServerConfigurer {
+@RequiredArgsConstructor
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+  private final AuthenticationManager authenticationManager;
+  private final UserDetailsService userDetailsService;
 
   @Override
-  public void configure(AuthorizationServerSecurityConfigurer authorizationServerSecurityConfigurer)
-      throws Exception {
-
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients
+        .inMemory()
+        .withClient("eagleeye")
+        .secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("thisissecret"))
+        .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+        .scopes("webclient", "mobileclient");
   }
 
   @Override
-  public void configure(ClientDetailsServiceConfigurer clientDetailsServiceConfigurer)
-      throws Exception {
-
-  }
-
-  @Override
-  public void configure(
-      AuthorizationServerEndpointsConfigurer authorizationServerEndpointsConfigurer)
-      throws Exception {
-
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+    endpoints
+        .authenticationManager(authenticationManager)
+        .userDetailsService(userDetailsService);
   }
 }
