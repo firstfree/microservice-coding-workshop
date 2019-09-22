642924c3dd63ff2b8ccdf8aed676c67a694a4249
ifjso <ojsboy@hotmail.com>
2019-09-22 16:25:53 +0900

    request interceptor 설정 추가

--- chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/config/RequestConfiguration.java
+++
@@ -1,5 +0,0 @@
-package com.thoughtmechanix.licenses.config;
-
-public class RequestConfiguration {
-
-}
---
+++ chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/config/RequestInterceptorConfiguration.java
@@ -0,0 +1,22 @@
+package com.thoughtmechanix.licenses.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+
+@Configuration
+public class RequestConfiguration {
+
+  @Bean
+  public RequestInterceptor requestInterceptor() {
+    return requestTemplate -> {
+      OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) SecurityContextHolder
+          .getContext()
+          .getAuthentication()
+          .getDetails();
+      requestTemplate.header("Authorization", "Bearer " + details.getTokenValue());
+    };
+  }
+}
