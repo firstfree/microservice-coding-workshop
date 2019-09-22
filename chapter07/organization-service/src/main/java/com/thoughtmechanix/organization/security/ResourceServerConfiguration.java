eaa1de0a51e8f727784cd24cc8abdeb1a64f7e40
ifjso <ojsboy@hotmail.com>
2019-09-22 15:32:08 +0900

    delete api 접근 제한 추가

--- chapter07/organization-service/src/main/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
+++ chapter07/organization-service/src/main/java/com/thoughtmechanix/organization/security/ResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.thoughtmechanix.organization.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -11,6 +12,8 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
   public void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
+        .antMatchers(HttpMethod.DELETE, "/v1/organizations/**")
+        .hasRole("ADMIN")
         .anyRequest()
         .authenticated();
   }
