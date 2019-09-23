44b8e5fda5d6d88fd3bd24e682ff148989a79bdc
ifjso <ojsboy@hotmail.com>
2019-09-23 22:00:04 +0900

    user organization repo 추가

---
+++ chapter07/authentication-service/src/main/java/com/thoughtmechanix/authentication/repository/OrgUserRepository.java
@@ -0,0 +1,9 @@
+package com.thoughtmechanix.authentication.repository;
+
+import com.thoughtmechanix.authentication.model.UserOrganization;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrgUserRepository extends CrudRepository<UserOrganization, String> {
+}
