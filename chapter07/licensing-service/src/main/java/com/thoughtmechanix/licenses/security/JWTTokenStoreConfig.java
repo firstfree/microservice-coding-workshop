ba6b1a2232523abc869ec45e2cf07a396260b1f2
ifjso <ojsboy@hotmail.com>
2019-09-23 21:38:51 +0900

    jwt token store config 추가

---
+++ chapter07/licensing-service/src/main/java/com/thoughtmechanix/licenses/security/JWTTokenStoreConfig.java
@@ -0,0 +1,39 @@
+package com.thoughtmechanix.licenses.security;
+
+import com.thoughtmechanix.licenses.config.ServiceConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@RequiredArgsConstructor
+public class JWTTokenStoreConfig {
+
+  private final ServiceConfig serviceConfig;
+
+  @Bean
+  @Primary
+  public DefaultTokenServices defaultTokenServices() {
+    DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+    defaultTokenServices.setTokenStore(tokenStore());
+    defaultTokenServices.setSupportRefreshToken(true);
+    return defaultTokenServices;
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JwtTokenStore(jwtAccessTokenConverter());
+  }
+
+  @Bean
+  public JwtAccessTokenConverter jwtAccessTokenConverter() {
+    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+    converter.setSigningKey(serviceConfig.getJwtSigningKey());
+    return converter;
+  }
+}
