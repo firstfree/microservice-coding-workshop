a445d0b97f398efb8cff9f33aa69efcd53f3ed29
ifjso <ojsboy@hotmail.com>
2019-09-30 08:14:23 +0900

    organization repository 추가

---
+++ chapter08/organization-service/src/main/java/com/thoughtmechanix/organization/repository/OrganizationRepository.java
@@ -0,0 +1,9 @@
+package com.thoughtmechanix.organization.repository;
+
+import com.thoughtmechanix.organization.model.Organization;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizationRepository extends CrudRepository<Organization, String> {
+}
