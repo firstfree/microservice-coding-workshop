9f101d4f4e165f2902f2818be17ee1056233c402
ifjso <ojsboy@hotmail.com>
2019-09-30 08:54:07 +0900

    user context 추가

---
+++ chapter08/organization-service/src/main/java/com/thoughtmechanix/organization/utils/UserContextHolder.java
@@ -0,0 +1,28 @@
+package com.thoughtmechanix.organization.utils;
+
+import org.springframework.util.Assert;
+
+public class UserContextHolder {
+
+  public static final ThreadLocal<UserContext> userContext = new ThreadLocal<>();
+
+  public static final UserContext getContext() {
+    UserContext context = userContext.get();
+
+    if (context == null) {
+      context = createEmptyContext();
+      userContext.set(context);
+    }
+
+    return userContext.get();
+  }
+
+  public static final UserContext createEmptyContext() {
+    return new UserContext();
+  }
+
+  public static void setContext(UserContext context) {
+    Assert.notNull(context, "Only non-null UserContext instance are permitted.");
+    userContext.set(context);
+  }
+}
