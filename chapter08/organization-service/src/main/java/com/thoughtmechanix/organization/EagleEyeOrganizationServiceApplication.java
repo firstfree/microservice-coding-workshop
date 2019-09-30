8c24b8adea46f0dfdfcd96b4249ed0d3b5f19359
ifjso <ojsboy@hotmail.com>
2019-09-30 09:35:14 +0900

    stream binding

--- chapter08/organization-service/src/main/java/com/thoughtmechanix/organization/EagleEyeOrganizationServiceApplication.java
+++ chapter08/organization-service/src/main/java/com/thoughtmechanix/organization/EagleEyeOrganizationServiceApplication.java
@@ -3,9 +3,12 @@ package com.thoughtmechanix.organization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableBinding(Source.class)
 public class EagleEyeOrganizationServiceApplication {
 
   public static void main(String[] args) {
