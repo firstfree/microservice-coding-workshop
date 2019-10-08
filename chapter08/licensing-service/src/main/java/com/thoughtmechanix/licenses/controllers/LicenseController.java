107a1cfeb504638e8eb5aa6fca2d0245ee450b55
ifjso <ojsboy@hotmail.com>
2019-10-09 06:06:48 +0900

    license controller에 insert, update, delete 추가

--- chapter08/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseController.java
+++ chapter08/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseController.java
@@ -6,10 +6,16 @@ import com.thoughtmechanix.licenses.utils.UserContextHolder;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -34,5 +40,22 @@ public class LicenseController {
     log.debug("LicenseController correlation id: {}, thread id: {}", correlationId,
         Thread.currentThread().getId());
     return licenseService.getLicense(organizationId, licenseId);
+  }
+
+  @PostMapping
+  public void saveLicense(@RequestBody License license) {
+    licenseService.saveLicense(license);
+  }
+
+  @PutMapping("/{licenseId}")
+  public void updateLicense(@PathVariable("licenseId") String licenseId,
+      @RequestBody License license) {
+    licenseService.updateLicense(license.withId(licenseId));
+  }
+
+  @DeleteMapping("/{licenseId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteLicense(@PathVariable("licenseId") String licenseId) {
+    licenseService.deleteLicense(licenseId);
   }
 }
