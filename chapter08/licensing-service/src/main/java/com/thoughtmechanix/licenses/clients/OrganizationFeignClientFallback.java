021157f7abaf67ff7a6962cfe87a9d9dd208865a
ifjso <ojsboy@hotmail.com>
2019-10-08 22:48:11 +0900

    organization feign client 추가

---
+++ chapter08/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationFeignClientFallback.java
@@ -0,0 +1,23 @@
+package com.thoughtmechanix.licenses.clients;
+
+import com.thoughtmechanix.licenses.model.Organization;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrganizationFeignClientFallback implements OrganizationFeignClient {
+
+  @Override
+  public Organization getOrganization(String organizationId) {
+    log.debug("getOrganization fallback called");
+
+    return Organization.builder()
+        .id("0000")
+        .name("name")
+        .contactName("contactName")
+        .contactEmail("contactEmail")
+        .contactPhone("contactPhone")
+        .build();
+  }
+}
