618d6b565e7bbb5f8045e3c25e9e484ff158baae
ifjso <ojsboy@hotmail.com>
2019-10-09 09:13:58 +0900

    custom channel 추가

--- chapter08/licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
+++ chapter08/licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
@@ -6,15 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Sink;
 
 @Slf4j
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableBinding(Sink.class)
 public class EagleEyeLicensingServiceApplication {
 
 	public static void main(String[] args) {
