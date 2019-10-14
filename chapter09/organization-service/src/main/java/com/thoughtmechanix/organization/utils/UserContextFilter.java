bd087ed1a510ad2e3e80ffb3f3c095d716f2a535
ifjso <ojsboy@hotmail.com>
2019-10-14 23:23:02 +0900

    UserContext 추가

---
+++ chapter09/organization-service/src/main/java/com/thoughtmechanix/organization/utils/UserContextFilter.java
@@ -0,0 +1,35 @@
+package com.thoughtmechanix.organization.utils;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UserContextFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+      FilterChain filterChain) throws IOException, ServletException {
+    log.debug("Entering the UserContextFilter for the organization service.");
+
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+    UserContext context = UserContextHolder.getContext();
+    context.setCorrelationId(request.getHeader(UserContext.CORRELATION_ID));
+    context.setUserId(request.getHeader(UserContext.USER_ID));
+    context.setAuthToken(request.getHeader(UserContext.AUTH_TOKEN));
+    context.setOrgId(request.getHeader(UserContext.ORG_ID));
+
+    log.debug("UserContextFilter correlation id: {}", context.getCorrelationId());
+    log.debug("Exiting the UserContextFilter");
+
+    filterChain.doFilter(servletRequest, servletResponse);
+  }
+}
