87b9cf82689b49a5ae0d85de054e078b9f1f3cb6
ifjso <ojsboy@hotmail.com>
2019-10-14 23:37:27 +0900

    message model 추가

---
+++ chapter09/organization-service/src/main/java/com/thoughtmechanix/organization/events/source/SimpleSourceBean.java
@@ -0,0 +1,33 @@
+package com.thoughtmechanix.organization.events.source;
+
+import com.thoughtmechanix.organization.events.models.OrganizationChangeModel;
+import com.thoughtmechanix.organization.utils.UserContextHolder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SimpleSourceBean {
+
+  private final Source source;
+
+  public void publishOrgChange(String action, String orgId) {
+    log.debug("Sending Kafka message {} for organization id/: {} correlation id: {}",
+        action, orgId, UserContextHolder.getContext().getCorrelationId());
+
+    OrganizationChangeModel change = OrganizationChangeModel.builder()
+        .type(OrganizationChangeModel.class.getTypeName())
+        .action(action)
+        .organizationId(orgId)
+        .correlationId(UserContextHolder.getContext().getCorrelationId())
+        .build();
+
+    source
+        .output()
+        .send(MessageBuilder.withPayload(change).build());
+  }
+}
