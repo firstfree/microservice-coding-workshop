4e4252663bf207ba54a2e2740a2494149848de6b
ifjso <ojsboy@hotmail.com>
2019-10-15 00:27:20 +0900

    eureka client, feign client, circuit breaker 추가

--- chapter09/licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
+++ chapter09/licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
@@ -2,12 +2,17 @@ package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
 public class EagleEyeLicensingServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EagleEyeLicensingServiceApplication.class, args);
   }
-
 }
