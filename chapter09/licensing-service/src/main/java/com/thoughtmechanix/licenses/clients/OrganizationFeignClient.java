42653cdb9c0960c4c2b0b8cd2453dbf1a7be1be2
ifjso <ojsboy@hotmail.com>
2019-10-16 00:35:34 +0900

    feign fallback, configuration 적용

--- chapter09/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationFeignClient.java
+++ chapter09/licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationFeignClient.java
@@ -1,9 +1,13 @@
 package com.thoughtmechanix.licenses.clients;
 
+import com.thoughtmechanix.licenses.config.RequestInterceptorConfiguration;
 import com.thoughtmechanix.licenses.model.Organization;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@FeignClient(name = "zuulservice", fallback = OrganizationFeignClientFallback.class,
+    configuration = RequestInterceptorConfiguration.class)
 public interface OrganizationFeignClient {
 
   @GetMapping("/api/organization/v1/organizations/{organizationId}")
