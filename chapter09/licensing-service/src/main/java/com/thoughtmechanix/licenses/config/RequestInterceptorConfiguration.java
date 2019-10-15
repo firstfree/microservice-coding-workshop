4912ff9007afe958841c412cdbf112de0041e484
ifjso <ojsboy@hotmail.com>
2019-10-15 09:05:42 +0900

    Configuration 추가

---
+++ chapter09/licensing-service/src/main/java/com/thoughtmechanix/licenses/config/RequestInterceptorConfiguration.java
@@ -0,0 +1,21 @@
+package com.thoughtmechanix.licenses.config;
+
+import com.thoughtmechanix.licenses.utils.UserContext;
+import com.thoughtmechanix.licenses.utils.UserContextHolder;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RequestInterceptorConfiguration {
+
+  @Bean
+  public RequestInterceptor requestInterceptor() {
+    return requestTemplate ->
+        requestTemplate.header(UserContext.CORRELATION_ID, getCorrelationId());
+  }
+
+  private String getCorrelationId() {
+    return UserContextHolder.getContext().getCorrelationId();
+  }
+}
---
+++ chapter09/licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -0,0 +1,29 @@
+package com.thoughtmechanix.licenses.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+
+  @Value("${example.property}")
+  private String exampleProperty;
+
+  @Value("${redis.server}")
+  private String redisServer;
+
+  @Value("${redis.port}")
+  private String redisPort;
+
+  public String getExampleProperty() {
+    return exampleProperty;
+  }
+
+  public String getRedisServer() {
+    return redisServer;
+  }
+
+  public Integer getRedisPort() {
+    return Integer.parseInt(redisPort);
+  }
+}
